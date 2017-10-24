815ea87bb15302f3cfa29c8e19a85e9a0b760016
Anonymous User <anon@anon-user.com>
2017-10-23 21:06:09 -0600

    sizing try 2

--- web/project01/signin.php
+++ web/project01/signin.php
@@ -26,16 +26,6 @@ include('login.php');
 	<!-- Latest compiled and minified JavaScript -->
 	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js" integrity="sha384-0mSbJDEHialfmuBBQP6A4Qrprq5OVfW37PRR3j5ELqxss1yVqOtnepnHVP9aJ7xS" crossorigin="anonymous"></script>
 
-	<script> 
-
-    $.get("/menuBar.html", function(data) {
-
-		$("#menuBar").html(data);
-
-	});
-
-    var page = "pageNotepad";
-    </script> 
 
     <style type="text/css">
 
@@ -67,9 +57,6 @@ include('login.php');
 
   </head>
 <body>
-
-<div id="menuBar"></div>
-
 
 <div class="container text-center" id="main">  
 <div class="row">
