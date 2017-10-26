b192e090c24e5e98681f707a98c2ed3c9e2960ea
Anonymous User <anon@anon-user.com>
2017-10-25 20:33:55 -0600

    reactivate db connection

--- web/project01/connection.php
+++ web/project01/connection.php
@@ -1,17 +1,17 @@
 <?php
 
-//$link = mysqli_connect("shareddb1a.hosting.stackcp.net", "cl59-journaldbd7", "dVceT/3XJ", "cl59-journaldbd7");
+$link = mysqli_connect("shareddb1a.hosting.stackcp.net", "cl59-journaldbd7", "dVceT/3XJ", "cl59-journaldbd7");
 
-// $dbUrl = getenv('DATABASE_URL');
+$dbUrl = getenv('DATABASE_URL');
 
-// $dbopts = parse_url($dbUrl);
+$dbopts = parse_url($dbUrl);
 
-// $dbHost = $dbopts["host"];
-// $dbPort = $dbopts["port"];
-// $dbUser = $dbopts["user"];
-// $dbPassword = $dbopts["pass"];
-// $dbName = ltrim($dbopts["path"],'/');
+$dbHost = $dbopts["host"];
+$dbPort = $dbopts["port"];
+$dbUser = $dbopts["user"];
+$dbPassword = $dbopts["pass"];
+$dbName = ltrim($dbopts["path"],'/');
 
-// $db = new PDO("pgsql:host=$dbHost;port=$dbPort;dbname=$dbName", $dbUser, $dbPassword);
+$db = new PDO("pgsql:host=$dbHost;port=$dbPort;dbname=$dbName", $dbUser, $dbPassword);
 
 // ?>
